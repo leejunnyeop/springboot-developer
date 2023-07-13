e02bc4f3909089f851fdec523dbb70b3652360f5
leejunnyeop <daffodil8620@naver.com>
2023-07-13 23:53:22 +0900

    init commit

---
+++ src/main/java/springbootdeveloper/junnyeop/JunnyeopApplication.java
@@ -0,0 +1,13 @@
+package springbootdeveloper.junnyeop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunnyeopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunnyeopApplication.class, args);
+	}
+
+}
